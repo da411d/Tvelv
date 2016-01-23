8e6f31c6e355f3bc5ed742c024fb6ffdea7182e2
Manzhula David <da411d@gmail.com>
2016-01-23 23:15:20 +0200

    Додав захист від брутфорса

--- functions/user.php
+++ functions/user.php
@@ -1,6 +1,7 @@
 <?		
 //############USER REGISTER############
 function isPasswordCorrect($login, $password){
+	sleep(3);
 	$database = db_connect();
 
 	$test = db_get("users", ["Login","Password","Salt"], ["Login[=]" => $login]);
