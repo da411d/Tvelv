f04c13702afb229a51aa08491eff3e723c54d46a
Manzhula David <da411d@gmail.com>
2016-01-24 21:35:35 +0200

    Додав можливість надсилання резервних копій через E-mail
    Тепер можна надсилати бекапи на мейл!

--- db/backup.php
+++ db/backup.php
@@ -3,6 +3,21 @@
 //****Цей файл треба поставити на cron з інтервалом  ~1 день
 //********************************************************//
 
+//Заголовок повідомлення
+$thm     = 'Tvelv backup '.date("d/m/Y H:i");
+
+//Текст повідомлення
+$msg     = 'Резервна копія Бази Даних системи "Tvelv"';
+
+//Отримувач
+$mail_to = 'example@example.com';
+
+
+
+
+
+
+
 include "../_functions.php";
 include "../_config.php";
 
@@ -16,5 +31,58 @@ if (copy($file, $newfile)){
 }else{
 	fwrite($t, "\r\n".$d.": Failed");
 }
+
+
+$file = "";
+$file = 'Tvelv_'.DB_SECRET.'.db';
+if (empty($file)){
+	if(mail($mail_to, $thm, $msg)){
+		fwrite($t, "\r\n".$d.": E-mail send complete");
+	}else{
+		fwrite($t, "\r\n".$d.": E-mail send failed");
+	}
+}else{
+	if(send_mail($mail_to, $thm, $msg, $file)){
+		fwrite($t, "\r\n".$d.": E-mail send complete");
+	}else{
+		fwrite($t, "\r\n".$d.": E-mail send failed");
+	}
+}
 fclose($t);
+
+function send_mail($mail_to, $thema, $html, $path){
+	if ($path) {
+		$fp = fopen($path, "rb");
+		if (!$fp) {
+			print "Cannot open file";
+			exit();
+		}
+		$file = fread($fp, filesize($path));
+		fclose($fp);
+	}
+	$name     = 'Backup-'.date("Y-m-d_H-i").'.db';
+	$EOL      = "\r\n";
+	$boundary = "--" . md5(uniqid(time()));
+	$headers  = "MIME-Version: 1.0;$EOL";
+	$headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"$EOL";
+	$headers .= "From: address@server.com";
+	$multipart = "--$boundary$EOL";
+	$multipart .= "Content-Type: text/html; charset=windows-1251$EOL";
+	$multipart .= "Content-Transfer-Encoding: base64$EOL";
+	$multipart .= $EOL;
+	$multipart .= chunk_split(base64_encode($html));
+	$multipart .= "$EOL--$boundary$EOL";
+	$multipart .= "Content-Type: application/octet-stream; name=\"$name\"$EOL";
+	$multipart .= "Content-Transfer-Encoding: base64$EOL";
+	$multipart .= "Content-Disposition: attachment; filename=\"$name\"$EOL";
+	$multipart .= $EOL;
+	$multipart .= chunk_split(base64_encode($file));
+	$multipart .= "$EOL--$boundary--$EOL";
+	if (!mail($mail_to, $thema, $multipart, $headers)) {
+		return False;
+	} else {
+		return True;
+	}
+	exit;
+}
 ?>
