42acce7f630f0e464a3054c527266124f551be23
Manzhula David <da411d@gmail.com>
2016-01-25 12:24:28 +0200

    Поправив футер

--- templates/_footer.php
+++ templates/_footer.php
@@ -1,4 +1,3 @@
 <?php error_reporting(0);include dirname(__FILE__)."/_ad.php";?>
 <a href="http://<?=SERVER_NAME;?>">©Tvelv</a><br>
-<a href="/index">Головна</a>|<a href="/about">Про мене</a>|<a href="/works">Робо́ти</a>
-<a href="https://uahosting.com.ua/billing/pl.php?10676" alt="UAhosting" target="_blank" style="display: block;"><img src="http://blastorq.pp.ua/site/assets/images/uahosting.png" border="0"></a>
+<a href="/profile">Профіль</a>|<a href="/marks">Оцінки</a>|<a href="/stats">Статистика</a>|<a href="/settings">Налаштування</a>|<a href="/logout">Вихід</a>
