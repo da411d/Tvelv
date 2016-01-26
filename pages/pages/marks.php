bc51a41de0fa2286b11bd21a103ef4cb42db313a
Manzhula David <da411d@gmail.com>
2016-01-26 17:28:42 +0200

    WTF
    Щось гітхаб висне...
    Ща відміню

--- pages/pages/marks.php
+++ pages/pages/marks.php
@@ -131,7 +131,6 @@
 				if($_POST['Teacher'] AND $_POST['Teacher']!='false'){$arrOfParams['Teacher'] = $_POST['Teacher'];}
 				if($_POST['Mark']){$arrOfParams['Mark'] = $_POST['Mark'];}
 				$arr = getMarksByParams($arrOfParams);
-				print_r($arrOfParams);
 				if(!$arr){$arr=[];}
 				usort($arr, function($a, $b){
 					$t1 = strtotime($a['Date'].' '.$a['Time']);
