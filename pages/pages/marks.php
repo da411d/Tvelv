71a0c54952651eeeacdbb9d45736dd1761b3ffc4
Manzhula David <da411d@gmail.com>
2016-01-28 22:32:34 +0200

    Профіксив

--- pages/pages/marks.php
+++ pages/pages/marks.php
@@ -7,7 +7,7 @@
 	<a class="link" href="/marks/listing">Лістинг</a>
 	Тут можна переглянути всі оцінки, відфільтрувати по даті, предмету, учню, вчителю або оцінці.
 </div>
-
+<?if(isTeacher()){?>
 <div class="card cw2" onmousedown="CClick(this, event)">
 	<div class="img-placeholder">
 		<img src="/assets/images/red-pen.png">
@@ -15,3 +15,4 @@
 	<a class="link" href="/marks/mark">Поставити оцінку</a>
 	Тут можна поставити оцінку учню.
 </div>
+<?}?>
