d6bed89ed7f211a51f7efb2b02ab4d03bbc52e93
Manzhula David <da411d@gmail.com>
2016-03-05 18:31:56 +0200

    Обновив шифрування
    Обновив шифрування:
    Вчора, після комітнув замітив, що для взлома шифра треба перебрати 256
    комбінацій. Всього. Але вчора було пізно і я виправив сьогодні.
    Я красафчик ;) Як завжди)

--- functions/crypto228.php
+++ functions/crypto228.php
@@ -2,9 +2,17 @@
 function _crypt($str, $key){
 	$encoded = bin2hex($str);
 	$encoded = str_split($encoded, 2);
+
+
+	$key = md5($key);
+	for($i=0;$i<(count($encoded)/16)+1;$i++){
+		$key .= md5($key.$i);
+	}
+	$key = str_split($key, 2);
 	$returnment = '';
-	foreach($encoded as $a){
-		$returnment .= bin2hex(pack('H*', $a)^pack('H*', substr(md5($key),0,2)));
+	for ($i=0;$i<count($encoded);$i++){
+		$a = $encoded[$i];
+		$returnment .= bin2hex(pack('H*', $a)^pack('H*', $key[$i]));
 	}
 	$returnment = hex2bin($returnment);
 	$returnment = base64_encode($returnment);
@@ -18,7 +26,16 @@ function _decrypt($str, $key){
 	$str = base64_decode($str);
 	$str = bin2hex($str);
 	$returnment = '';
-	foreach(str_split($str, 2) as $a){
+	$str = str_split($str, 2);
+
+	$key = md5($key);
+	for($i=0;$i<(count($str)/16)+1;$i++){
+		$key .= md5($key.$i);
+	}
+	$key = str_split($key, 2);
+
+	for ($i=0;$i<count($str);$i++){
+		$a = $str[$i];
 		$returnment .= (
 			pack('H*', 
 				pack('H*', 
@@ -28,7 +45,7 @@ function _decrypt($str, $key){
 			^
 			pack('H*', 
 				pack('H*', 
-					bin2hex(substr(md5($key),0,2))
+					bin2hex($key[$i])
 				)
 			)
 		);
