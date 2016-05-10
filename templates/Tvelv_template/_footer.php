fc5d098a73e1c53a0d3b6077b8f30782016ccf0b
Manzhula David <da411d@gmail.com>
2016-05-10 12:22:48 +0300

    Видалив статистику

--- templates/Tvelv_template/_footer.php
+++ templates/Tvelv_template/_footer.php
@@ -1,2 +1,2 @@
 <a href="http://<?=SERVER_NAME;?>">©Tvelv</a><br>
-<a href="/profile">Профіль</a>|<a href="/marks">Оцінки</a>|<a href="/stats">Статистика</a>|<a href="/settings">Налаштування</a>|<a href="/logout">Вихід</a>
+<a href="/profile">Профіль</a>|<a href="/marks">Оцінки</a>|<a href="/settings">Налаштування</a>|<a href="/logout">Вихід</a>
--- templates/Tvelv_template/_header.php
+++ templates/Tvelv_template/_header.php
@@ -3,7 +3,6 @@
 	<nav id="nav">
 		<a href="/#profile" class="profiletop"><img src="/assets/images/icons/account.svg" class="icon">Профіль</a>
 		<a href="/#marks" class="markstop"><img src="/assets/images/icons/book-open.svg" class="icon">Оцінки</a>
-		<a href="/#stats" class="statstop"><img src="/assets/images/icons/chart-line.svg" class="icon">Статистика</a>
 		<a href="/#settings" class="settingstop"><img src="/assets/images/icons/settings.svg" class="icon">Налаштування</a>
 		<a href="/#logout?Hi_What_are_you_looking_for=<?=sha1(getPasswordSalt($code['a']));?>&rnd<?=rand(10000000,99999999);?>" class="logouttop"><img src="/assets/images/icons/logout.svg" class="icon">Вихід</a>
 	</nav>
