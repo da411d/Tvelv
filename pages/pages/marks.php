4a793f45008f62a417c932c336cd407c849928ff
Manzhula David <da411d@gmail.com>
2016-05-11 18:28:03 +0300

    Поправив меню з оцінками

--- pages/pages/marks.php
+++ pages/pages/marks.php
@@ -1,14 +1,9 @@
 <?$title="Оцінки";?>
-
-<div class="card cw2" onmousedown="CClick(this, event)">
-	<div class="img-placeholder">
-		<img src="/assets/images/12.png">
-	</div>
-	<a class="link" href="/#marks/listing">Лістинг</a>
-	Тут можна переглянути всі оцінки, відфільтрувати по даті, предмету, учню, вчителю або оцінці.
-</div>
+<?
+$cnt = isTeacher()?3:2;
+?>
 <?if(isTeacher()){?>
-<div class="card cw2" onmousedown="CClick(this, event)">
+<div class="card cw<?=$cnt;?>" onmousedown="CClick(this, event)">
 	<div class="img-placeholder">
 		<img src="/assets/images/red-pen.png">
 	</div>
@@ -16,3 +11,17 @@
 	Тут можна поставити оцінку учню.
 </div>
 <?}?>
+<div class="card cw<?=$cnt;?>" onmousedown="CClick(this, event)">
+	<div class="img-placeholder">
+		<img src="/assets/images/12.png">
+	</div>
+	<a class="link" href="/#marks/listing">Лістинг</a>
+	Тут можна переглянути всі оцінки, відфільтрувати по даті, предмету, учню, вчителю або оцінці.
+</div>
+<div class="card cw<?=$cnt;?>" onmousedown="CClick(this, event)">
+	<div class="img-placeholder">
+		<img src="/assets/images/12.png">
+	</div>
+	<a class="link" href="/#marks/journal">Журнал</a>
+	Тут оцінки як в журналі
+</div>
