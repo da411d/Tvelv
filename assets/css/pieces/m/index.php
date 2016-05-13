e84aa12b245b767bee0f7f7103a23c9ec2d5d465
Manzhula David <da411d@gmail.com>
2016-05-13 16:54:13 +0300

    Поправив стилі. Був якись баг. хз

--- assets/css/pieces/m/index.php
+++ assets/css/pieces/m/index.php
@@ -1,12 +1,15 @@
 <?
-$dh = opendir(dirname(__FILE__));
+$dh = opendir(dirname(__FILE__).'/');
 while (false !== ($filename = readdir($dh))) {
     $files[] = $filename;
 }
 sort($files);
-$arr = array();
+$includements = [];
 foreach ($files as $f) { 
-	if($f AND !strpos(' '.$f, '!') AND strpos($f, '.css') AND !strpos($f, '.php')  AND !is_dir($f)){
-		include $f;
+	if($f AND !strpos(' '.$f, '!') AND strpos($f, '.css') AND !strpos($f, '.php')  AND !is_dir(dirname(__FILE__).'/'.$f) AND file_exists(dirname(__FILE__).'/'.$f) AND !in_array($f, $includements)){
+		$includements[] = dirname(__FILE__).'/'.$f;
 	} 
+}
+foreach ($includements as $i) { 
+	include_once $i;
 }
